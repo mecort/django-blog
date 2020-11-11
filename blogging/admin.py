d071c96847ed274289cf6e57d03b8acfbf118cf2
Michael Cort <mcort@uw.edu>
2020-11-10 18:51:36 -0800

    Changed models to allow category selection at the post creation.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+# class CategoryAdmin(admin.ModelAdmin):
+#     # Fill me
+
+# class PostAdmin(admin.ModelAdmin):
+#     # Fill me
+
 admin.site.register(Post)
 admin.site.register(Category)
