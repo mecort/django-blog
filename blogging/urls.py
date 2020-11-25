9ffe5c0c4bdc8c3302afbcb6a56ca07fd5574c42
Ian Canavan <icanavan.327@gmail.com>
2020-11-24 18:32:38 -0800

    implemented rss feed

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blogging.views import BlogListView, BlogDetailView
+from blogging.views import BlogListView, BlogDetailView, LatestEntriesFeed
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("feed/", LatestEntriesFeed()),
 ]
